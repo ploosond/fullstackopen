0303eb8d02e8c68c60aa8e12e605687f8b7bb19f
Prajwol Devkota <prajwol.devkota016@gmail.com>
2024-10-27 14:59:06 +0100

    1.5: Course Information step 5 submission

--- part1/courseInformation/src/App.jsx
+++ part1/courseInformation/src/App.jsx
@@ -1,24 +1,26 @@
 const App = () => {
-  const course = "Half Stack application development"
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises1: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises2: 7,
-    },
-    {
-      name: "State of a component",
-      exercises3: 14,
-    },
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises1: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises2: 7,
+      },
+      {
+        name: "State of a component",
+        exercises3: 14,
+      },
+    ],
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
