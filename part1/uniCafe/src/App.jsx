a510d4967b29a291039c5091b1c1cb5f11ebb1bc
Prajwol Devkota <prajwol.devkota016@gmail.com>
2024-11-04 17:02:26 +0100

    1.7: unicafe step 2 submission

--- part1/uniCafe/src/App.jsx
+++ part1/uniCafe/src/App.jsx
@@ -4,17 +4,37 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allTotal, setAll] = useState(0)
+
+  const handleGood = () => {
+    const updatedValue = good + 1
+    setGood(updatedValue)
+    setAll(updatedValue + neutral + bad)
+  }
+  const handleNeutral = () => {
+    const updatedValue = neutral + 1
+    setNeutral(updatedValue)
+    setAll(good + updatedValue + bad)
+  }
+  const handleBad = () => {
+    const updatedValue = bad + 1
+    setBad(updatedValue)
+    setAll(good + neutral + updatedValue)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>good</button>
-      <button onClick={() => setBad(bad + 1)}>good</button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutal {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {allTotal}</p>
+      <p>average {(good - bad) / allTotal}</p>
+      <p>positive {(good / allTotal) * 100} %</p>
     </div>
   )
 }
