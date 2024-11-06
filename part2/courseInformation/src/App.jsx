a3c5712fdea092680194ce5b4cc1cceca175e102
Prajwol Devkota <prajwol.devkota016@gmail.com>
2024-11-06 21:10:25 +0100

    2.1: Course information step 6 submission

---
+++ part2/courseInformation/src/App.jsx
@@ -0,0 +1,50 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
+
+  return <Course course={course} />
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content content={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
+}
+
+const Content = ({ content }) => {
+  return content.map((part) => (
+    <Part key={part.id} name={part.name} exercises={part.exercises} />
+  ))
+}
+
+const Part = ({ name, exercises }) => {
+  return <p>{`${name} ${exercises}`}</p>
+}
+
+export default App
