481ff15f2e067d7ec094271e4de700014172fe28
Prajwol Devkota <prajwol.devkota016@gmail.com>
2024-11-06 21:46:05 +0100

    2.4: Course information step 9 submission

--- part2/courseInformation/src/App.jsx
+++ part2/courseInformation/src/App.jsx
@@ -1,46 +1,78 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  }
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
 
-  return <Course course={course} />
-}
-
-const Course = ({ course }) => {
   return (
     <div>
-      <Header name={course.name} />
-      <Content content={course.parts} />
-      <Total exercises={course.parts} />
+      <Header text="Web development curriculum" />
+      <Course courses={courses} />
+    </div>
+  )
+}
+const Header = ({ text }) => {
+  return <h1>{text}</h1>
+}
+
+const Course = ({ courses }) => {
+  return (
+    <div>
+      {courses.map((course) => {
+        return (
+          <div key={course.id}>
+            <CourseHeader name={course.name} />
+            <Content content={course.parts} />
+            <Total exercises={course.parts} />
+          </div>
+        )
+      })}
     </div>
   )
 }
 
-const Header = ({ name }) => {
-  return <h1>{name}</h1>
+const CourseHeader = ({ name }) => {
+  return <h2>{name}</h2>
 }
 
 const Content = ({ content }) => {
