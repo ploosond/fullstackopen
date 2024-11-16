91effbb51e4676ddfdd55fa18d8d2f316533f49b
Prajwol Devkota <prajwol.devkota016@gmail.com>
2024-11-16 23:51:49 +0100

    2.18* Data for countries, step 1 submission

---
+++ part2/dataforcountries/src/App.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [countries, setCountries] = useState([])
+  const [isTrue, setIsTrue] = useState(null)
+
+  useEffect(() => {
+    if (isTrue) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+        .then((response) => {
+          setCountries(response.data.map((c) => c.name.common))
+        })
+    }
+  }, [isTrue])
+
+  const handleChange = (event) => {
+    setInput(event.target.value)
+    setIsTrue(true)
+  }
+
+  const filteredCountries = countries.filter((c) =>
+    c.toLowerCase().includes(input.toLowerCase())
+  )
+
+  return (
+    <div>
+      find countries <input type="text" value={input} onChange={handleChange} />
+      {filteredCountries.length === 1 ? (
+        <CountryDetail country={filteredCountries[0]} />
+      ) : filteredCountries.length <= 10 ? (
+        filteredCountries.map((c, i) => <p key={i}>{c}</p>)
+      ) : (
+        <p>Too many matches, specify another filter</p>
+      )}
+    </div>
+  )
+}
+
+const CountryDetail = ({ country }) => {
+  const [details, setDetails] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+      .then((response) => {
+        setDetails(details.concat(response.data))
+      })
+  }, [])
+  return (
+    <div>
+      {details.map((c, i) => {
+        return (
+          <div key={i}>
+            <h1>{c.name.common}</h1>
+            <p>capital {c.capital}</p>
+            <p>area {c.area}</p>
+            <h4>languages:</h4>
+            <ul>
+              {Object.entries(c.languages).map(([i, l]) => (
+                <li key={i}>{l}</li>
+              ))}
+            </ul>
+            <img src={c.flags.svg} />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default App
