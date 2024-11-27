4cb63eaa5fe8f4224106a9a8203ec628088046c6
Prajwol Devkota <prajwol.devkota016@gmail.com>
2024-11-27 17:55:56 +0100

    4.2 Blog List, step 2 submission

---
+++ part4/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const blogsRouter = require('./controllers/blogs')
+
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB:', config.MONGODB_URI)
+  })
+  .catch((error) => {
+    logger.error(error.message)
+  })
+
+const app = express()
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.requestLogger)
+app.use(middleware.unknownRequest)
+app.use(middleware.errorHandler)
+
+module.exports = app
