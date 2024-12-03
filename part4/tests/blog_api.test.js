02410d85321bb130f3127a82919fe0d8589dc4c5
Prajwol Devkota <prajwol.devkota016@gmail.com>
2024-12-03 11:28:12 +0100

    4.10: Blog List Tests, step 3 submission

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -31,6 +31,29 @@ test.only('blog post unique identifier is named id, not _id', async () => {
   assert(!blogs.every((blog) => blog.hasOwnProperty('_id')))
 })
 
+test.only('POST request creates a new blog post', async () => {
+  const blogs = await helper.BlogsInDb()
+
+  const blog = {
+    title: 'Tips for developing E-commerce With NextJS',
+    author: 'Doe Rogue',
+    url: 'John Smith',
+    likes: 500,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map((blog) => blog.title)
+
+  assert.strictEqual(response.body.length, blogs.length + 1)
+  assert(contents.includes(blog.title))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
