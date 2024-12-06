7fe2dd2167ea5d231745024951e4adbeb198c3a0
Prajwol Devkota <prajwol.devkota016@gmail.com>
2024-12-06 21:51:59 +0100

    4.17: Blog List Expansion, step 5 submission

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,22 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   res.json(blogs)
 })
 
 blogsRouter.post('/', async (req, res) => {
-  const blog = new Blog(req.body)
-  if (!blog.likes) {
-    blog.likes = 0
-  }
+  const body = req.body
+  const user = await User.findById(body.userId)
 
-  if (!blog.title || !blog.url) {
-    return res.status(400).end()
-  }
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog)
+  await user.save()
+
   res.status(201).json(savedBlog)
 })
 
