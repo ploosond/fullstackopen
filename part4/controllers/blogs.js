435016f17d55e573dc6d5fdfeecbd1675f5f26f6
Prajwol Devkota <prajwol.devkota016@gmail.com>
2024-12-07 17:22:53 +0100

    4.19: Blog List Expansion, step 7 submission

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,21 +2,37 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   res.json(blogs)
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+
+  return null
+}
+
 blogsRouter.post('/', async (req, res) => {
   const body = req.body
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+  if (!decodedToken) {
+    return res.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user.id,
+    user: user._id,
   })
 
   const savedBlog = await blog.save()
