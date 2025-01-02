3a4e43d4de6a91606f475f8dbca66d86967b4a62
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-01-02 19:34:20 +0100

    fixed 6.7: Anecdotes, step 5

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
