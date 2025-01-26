bde9131c2c3e81e32bbc583b5b8647855529d9fb
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-01-26 20:20:24 +0100

    Exercise 6.21 submission

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -3,3 +3,8 @@ import axios from "axios";
 const baseUrl = "http://localhost:3001/anecdotes";
 
 export const getAnecdotes = () => axios.get(baseUrl).then((res) => res.data);
+
+export const createAnecdote = (newAnecdote) =>
+  axios
+    .post("http://localhost:3001/anecdotes", newAnecdote)
+    .then((res) => res.data);
