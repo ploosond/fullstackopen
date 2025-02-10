73dae3dbdcbf708398f5611b63646355d26e95aa
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-02-10 19:20:45 +0100

    fixed backend to return deleted blog when /delete route is triggered

--- part7/bloglist-react-query-context/backend/controllers/blogs.js
+++ part7/bloglist-react-query-context/backend/controllers/blogs.js
@@ -1,25 +1,25 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const middleware = require('../utils/middleware')
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const middleware = require('../utils/middleware');
 
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  res.json(blogs)
-})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
+  res.json(blogs);
+});
 
 blogsRouter.post('/', middleware.userExtractor, async (req, res) => {
-  const body = req.body
-  const decodedToken = jwt.verify(req.token, process.env.SECRET)
+  const body = req.body;
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
   if (!decodedToken) {
-    return res.status(401).json({ error: 'invalid token' })
+    return res.status(401).json({ error: 'invalid token' });
   }
   if (!body.title || !body.url) {
-    return res.status(400).json({ error: 'both title and url are required' })
+    return res.status(400).json({ error: 'both title and url are required' });
   }
 
-  const user = req.user
+  const user = req.user;
 
   const blog = new Blog({
     title: body.title,
@@ -27,46 +27,46 @@ blogsRouter.post('/', middleware.userExtractor, async (req, res) => {
     url: body.url,
     likes: body.likes || 0,
     user: user,
-  })
+  });
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
 
-  res.status(201).json(savedBlog)
-})
+  res.status(201).json(savedBlog);
+});
 
 blogsRouter.put('/:id', async (req, res) => {
-  const body = req.body
+  const body = req.body;
   const blog = {
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-  }
+  };
   const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
     new: true,
-  }).populate('user', { username: 1, name: 1 })
+  }).populate('user', { username: 1, name: 1 });
 
-  res.status(200).json(updatedBlog)
-})
+  res.status(200).json(updatedBlog);
+});
 
 blogsRouter.delete('/:id', middleware.userExtractor, async (req, res) => {
-  const blog = await Blog.findById(req.params.id)
-  const user = req.user
+  const blog = await Blog.findById(req.params.id);
+  const user = req.user;
 
-  const decodedToken = jwt.verify(req.token, process.env.SECRET)
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
   if (!decodedToken) {
-    res.status(401).json({ error: 'invalid token' })
+    res.status(401).json({ error: 'invalid token' });
   }
 
   if (blog.user.toString() !== user.id) {
     return res
       .status(401)
-      .json('Unauthorized request, blog was created by someone else')
+      .json('Unauthorized request, blog was created by someone else');
   }
-  await Blog.findByIdAndDelete(req.params.id)
-  res.status(204).end()
-})
+  const deletedBlog = await Blog.findByIdAndDelete(req.params.id);
+  res.status(200).json(deletedBlog);
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
