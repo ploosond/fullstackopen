df001458f8f97369ec9d1ee83cdcf21ca92d16de
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-02-17 22:26:06 +0100

    8.5: Books by genre

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -120,7 +120,7 @@ const typeDefs = `
   type Query {
   bookCount: Int!
   authorCount: Int!
-  allBooks(author: String): [Book!]!
+  allBooks(author: String, genre: String): [Book!]!
   allAuthors: [AuthorStats!]!
   }
 `;
@@ -130,10 +130,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books;
+      if (args.author) {
+        return books.filter((book) => book.author === args.author);
       }
-      return books.filter((book) => book.author === args.author);
+
+      if (args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre));
+      }
+
+      return books;
     },
     allAuthors: () => {
       return authors.map((author) => ({
