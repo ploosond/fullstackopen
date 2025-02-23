56a4235fd4376aa40d49ffcdd1d8f488a14df561
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-02-23 10:52:08 +0100

    8.15 Database, part 3

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -25,26 +25,21 @@ mongoose
 let authors = [
   {
     name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
     born: 1952,
   },
   {
     name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
     born: 1963,
   },
   {
     name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
     born: 1821,
   },
   {
     name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
   },
   {
     name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
   },
 ];
 
@@ -53,49 +48,42 @@ let books = [
     title: "Clean Code",
     published: 2008,
     author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
     genres: ["refactoring"],
   },
   {
     title: "Agile software development",
     published: 2002,
     author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
     genres: ["agile", "patterns", "design"],
   },
   {
     title: "Refactoring, edition 2",
     published: 2018,
     author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
     genres: ["refactoring"],
   },
   {
     title: "Refactoring to patterns",
     published: 2008,
     author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
     genres: ["refactoring", "patterns"],
   },
   {
     title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
     published: 2012,
     author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
     genres: ["refactoring", "design"],
   },
   {
     title: "Crime and punishment",
     published: 1866,
     author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
     genres: ["classic", "crime"],
   },
   {
     title: "Demons",
     published: 1872,
     author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
     genres: ["classic", "revolution"],
   },
 ];
@@ -144,16 +132,35 @@ const resolvers = {
     authorCount: async () => Author.collection.countDocuments(),
     allBooks: async (root, args) => {
       if (args.author && args.genre) {
-        const author = await Author.findOne({ name: args.author });
-        return Book.find({
-          author: author._id,
-          genres: { $in: [args.genre] },
-        }).populate("author");
+        try {
+          const author = await Author.findOne({ name: args.author });
+
+          return Book.find({
+            author: author._id,
+            genres: { $in: [args.genre] },
+          }).populate("author");
+        } catch (error) {
+          throw new GraphQLError("Author does not exists", {
+            extensions: {
+              code: "BAD_USER_INPUT",
+              invalidArgs: args.author,
+            },
+          });
+        }
       }
 
       if (args.author) {
-        const author = await Author.findOne({ name: args.author });
-        return Book.find({ author: author._id }).populate("author");
+        try {
+          const author = await Author.findOne({ name: args.author });
+          return Book.find({ author: author._id }).populate("author");
+        } catch (error) {
+          throw new GraphQLError("Author does not exists", {
+            extensions: {
+              code: "BAD_USER_INPUT",
+              invalidArgs: args.author,
+            },
+          });
+        }
       }
 
       if (args.genre) {
@@ -181,36 +188,57 @@ const resolvers = {
       let author = await Author.findOne({ name: args.author });
 
       if (!author) {
-        author = new Author({ name: args.author });
-        await author.save();
+        try {
+          author = new Author({ name: args.author });
+          await author.save();
+        } catch (error) {
+          throw new GraphQLError(
+            "Author should be minimum 4 characters length",
+            {
+              extensions: {
+                code: "BAD_USER_INPUT",
+                invalidArgs: args.author,
+              },
+            }
+          );
+        }
       }
 
       let newBook = new Book({ ...args, author: author._id });
-      await newBook.save();
+      try {
+        await newBook.save();
+      } catch (error) {
+        throw new GraphQLError("Title should be minimum 5 characters length", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.author,
+          },
+        });
+      }
 
       newBook = await Book.findById(newBook._id).populate("author");
       return newBook;
     },
     editAuthor: async (root, args) => {
-      const authorExists = await Author.findOne({ name: args.name });
+      try {
+        const authorExists = await Author.findOne({ name: args.name });
 
-      if (!authorExists) {
+        const updatedAuthor = await Author.findOneAndUpdate(
+          { name: authorExists.name },
+          { born: args.setBornTo },
+          { new: true }
+        );
+
+        return updatedAuthor;
+      } catch (error) {
         throw new GraphQLError("Author does not exists", {
           extensions: {
             code: "BAD_USER_INPUT",
-            invalidArgs: args.name,
+            invalidArgs: args.author,
             error,
           },
         });
       }
-
-      const updatedAuthor = await Author.findOneAndUpdate(
-        { name: authorExists.name },
-        { born: args.setBornTo },
-        { new: true }
-      );
-
-      return updatedAuthor;
     },
   },
 };
