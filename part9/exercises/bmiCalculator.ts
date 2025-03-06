16a1604e093f036f37fa565fffa033a54fe32c8b
Prajwol Devkota <prajwol.devkota016@gmail.com>
2025-03-06 11:50:02 +0100

    9.5 WebBMI

--- part9/exercises/bmiCalculator.ts
+++ part9/exercises/bmiCalculator.ts
@@ -3,7 +3,7 @@ interface Inputs {
   value2: number;
 }
 
-const parseArguments = (args: string[]): Inputs => {
+export const parseArguments = (args: string[]): Inputs => {
   if (args.length < 4) throw new Error("Not enough arguments");
   if (args.length > 4) throw new Error("Too many arguments");
 
@@ -17,37 +17,44 @@ const parseArguments = (args: string[]): Inputs => {
   }
 };
 
-const calculateBmi = (height: number, weight: number) => {
+export const calculateBmi = (
+  height: number,
+  weight: number
+): string | undefined => {
   height /= 100;
 
   const result = weight / (height * height);
 
   if (result < 16.0) {
-    console.log("Underweight (Severe thinness) ");
+    return "Underweight (Severe thinness) ";
   } else if (result > 16.0 && result < 16.9) {
-    console.log("Underweight (Moderate thinness)");
+    return "Underweight (Moderate thinness)";
   } else if (result > 17.0 && result < 18.4) {
-    console.log("Underweight (Mild thinness)");
+    return "Underweight (Mild thinness)";
   } else if (result > 18.5 && result < 24.9) {
-    console.log("Normal range");
+    return "Normal range";
   } else if (result > 25.0 && result < 29.9) {
-    console.log("Overweight");
+    return "Overweight";
   } else if (result > 30.0 && result < 34.9) {
-    console.log("Obese (Class I)");
+    return "Obese (Class I)";
   } else if (result > 35.0 && result < 39.9) {
-    console.log("Obese (Class II)");
+    return "Obese (Class II)";
   } else if (result >= 40.0) {
-    console.log("Obese (Class III) ");
+    return "Obese (Class III) ";
   }
+
+  return undefined;
 };
 
-try {
-  const { value1, value2 } = parseArguments(process.argv);
-  calculateBmi(value1, value2);
-} catch (error: unknown) {
-  let errorMessage = "Something bad happened";
-  if (error instanceof Error) {
-    errorMessage += "Error: " + error.message;
+if (require.main === module) {
+  try {
+    const { value1, value2 } = parseArguments(process.argv);
+    console.log(calculateBmi(value1, value2));
+  } catch (error: unknown) {
+    let errorMessage = "Something bad happened";
+    if (error instanceof Error) {
+      errorMessage += "Error: " + error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
--- part9/exercises/index.ts
+++ part9/exercises/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack");
+});
+
+app.get("/bmi", (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (!isNaN(height) && !isNaN(weight)) {
+    const result = calculateBmi(height, weight);
+    res.json({ weight, height, bmi: result });
+  } else {
+    res.json({
+      error: " malformatted parameters",
+    });
+  }
 });
 
 const PORT = 3003;
